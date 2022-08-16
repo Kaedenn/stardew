ca7e35dc730693c72b85ed2936bc6787ba984967
Kaedenn <kaedenn@gmail.com>
2022-08-16 14:40:24 -0800

    Move forage information to a file

--- stardew.py
+++ stardew.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
 
 """
-Constants and functions related to Stardew Valley
+Constants and functions related to Stardew Valley game data
+
+This module provides access to the several names, locations, and objects
+defined in the game. Exported identifiers include the following:
+
+NPCS            tuple of all NPCs
+LOCATIONS       tuple of all location types (or names; see below)
+OBJECTS         dict of object ID to object definition (see help(stardew.Data))
+FORAGE          tuple containing names of all forage objects
+FORAGE_<set>    tuple containing names of the particular forage set
+
+Note on NPCS and LOCATIONS values: these values refer to the xsi:type
+attributes, if present, and the Name child element otherwise. See the README
+for determining the name of a node.
 """
 
 import enum
@@ -11,14 +24,23 @@ import os
 DATA_PATH = "data"
 
 def _load_data(name, reader=None):
-  "Read the given data file"
+  """
+  Read the given data file.
+
+  If reader is None, this returns file_object.read().splitlines()
+  Otherwise, this returns reader(file_object)
+  """
   with open(os.path.join(DATA_PATH, name), "rt") as fobj:
     if reader is None:
       return fobj.read().splitlines()
     return reader(fobj)
 
 class Data(enum.Enum):
-  "Object field names"
+  """
+  Object definition keys
+
+  Use these when accessing OBJECT[oid] fields.
+  """
   NAME = "name"
   VALUE = "value"
   EDIBILITY = "edibility"
@@ -29,7 +51,7 @@ class Data(enum.Enum):
   EXTRAS = "extras"
 
 def _parse_object(odef):
-  "Parse a raw object definition"
+  "Parse a raw object definition into a dict with the above keys"
   fields = odef.split("/")
   name = fields[0]
   value = fields[1]
@@ -62,6 +84,17 @@ NPCS = tuple(_load_data("npcs.txt")) + (NPC_UNKNOWN,)
 LOCATIONS = tuple(_load_data("locations.txt")) + (LOCATION_UNKNOWN,)
 OBJECTS_RAW = _load_data("objects.json", reader=json.load)
 OBJECTS = {oid: _parse_object(odef) for oid, odef in OBJECTS_RAW.items()}
+FORAGE_SETS = _load_data("forage.json", reader=json.load)
+
+FORAGE_SPRING = tuple(FORAGE_SETS["spring"])
+FORAGE_SUMMER = tuple(FORAGE_SETS["summer"])
+FORAGE_FALL = tuple(FORAGE_SETS["fall"])
+FORAGE_WINTER = tuple(FORAGE_SETS["winter"])
+FORAGE_BEACH = tuple(FORAGE_SETS["beach"])
+FORAGE_MINES = tuple(FORAGE_SETS["mines"])
+FORAGE_DESERT = tuple(FORAGE_SETS["desert"])
+FORAGE_ISLAND = tuple(FORAGE_SETS["island"])
+FORAGE = tuple(set.union(*(set(v) for v in FORAGE_SETS.values())))
 
 def get_object(oid, fallback=None, field=None):
   "Get the object with the given ID"
@@ -73,88 +106,5 @@ def get_object(oid, fallback=None, field=None):
       return obj[field]
     return obj
   return fallback
-
-# Map Object Sets {{{0
-
-FORAGE_SPRING = (
-  "Wild Horseradish",
-  "Daffodil",
-  "Leek",
-  "Dandelion",
-  "Spring Onion",
-  "Common Mushroom",
-  "Morel",
-  "Salmonberry"
-)
-
-FORAGE_SUMMER = (
-  "Grape",
-  "Spice Berry",
-  "Sweet Pea",
-  "Red Mushroom",
-  "Fiddlehead Fern",
-  "Common Mushroom"
-)
-
-FORAGE_FALL = (
-  "Common Mushroom",
-  "Wild Plum",
-  "Hazelnut",
-  "Blackberry",
-  "Chanterelle",
-  "Red Mushroom",
-  "Purple Mushroom"
-)
-
-FORAGE_WINTER = (
-  "Winter Root",
-  "Crystal Fruit",
-  "Snow Yam",
-  "Crocus",
-  "Holly"
-)
-
-FORAGE_BEACH = (
-  "Nautilus Shell",
-  "Coral",
-  "Sea Urchin",
-  "Rainbow Shell",
-  "Clam",
-  "Cockle",
-  "Mussel",
-  "Oyster",
-  "Seaweed"
-)
-
-FORAGE_MINES = (
-  "Red Mushroom",
-  "Purple Mushroom",
-  "Cave Carrot"
-)
-
-FORAGE_DESERT = (
-  "Cactus Fruit",
-  "Coconut"
-)
-
-FORAGE_ISLAND = (
-  "Ginger",
-  "Magma Cap"
-)
-
-FORAGE = set(
-  FORAGE_SPRING +
-  FORAGE_SUMMER +
-  FORAGE_FALL +
-  FORAGE_WINTER +
-  FORAGE_BEACH +
-  FORAGE_MINES +
-  FORAGE_DESERT +
-  FORAGE_ISLAND
-)
-
-# 0}}}
-
-
 
 # vim: set ts=2 sts=2 sw=2:
