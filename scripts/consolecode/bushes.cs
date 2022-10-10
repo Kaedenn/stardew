ec86253afb66d342e31ff12818f9750a258f96dd
Kaedenn <kaedenn@gmail.com>
2022-10-09 17:22:41 -0800

    Scan all locations and output debug warp commands

--- scripts/consolecode/bushes.cs
+++ scripts/consolecode/bushes.cs
@@ -1,12 +1,40 @@
 
 /* Display locations of all berry bushes in the current map */
 {
-  IEnumerable<StardewValley.TerrainFeatures.LargeTerrainFeature> bushes =
-    Game1.player.currentLocation.largeTerrainFeatures.Where(
-      x => x is StardewValley.TerrainFeatures.Bush b
+  bool currMap = true;
+
+  foreach (var arg in args) {
+    if (arg == "-a") {
+      currMap = false;
+    }
+  }
+
+  var bmap = new Dictionary<string, List<StardewValley.TerrainFeatures.LargeTerrainFeature>>();
+  var blist = new List<Tuple<string, StardewValley.TerrainFeatures.LargeTerrainFeature>>();
+
+  foreach (var loc in Game1.locations) {
+    if (currMap && loc != Game1.currentLocation) continue;
+    foreach (var bush in loc.largeTerrainFeatures.Where(
+      feat => feat is StardewValley.TerrainFeatures.Bush b
         && b.inBloom(Game1.currentSeason, Game1.dayOfMonth)
         && !b.townBush
-        && b.tileSheetOffset == 1);
+        && b.tileSheetOffset == 1)) {
+      if (!bmap.ContainsKey(loc.name)) {
+        bmap[loc.name] = new List<StardewValley.TerrainFeatures.LargeTerrainFeature>();
+      }
+      bmap[loc.name].Add(bush);
+      blist.Add(new Tuple<string, StardewValley.TerrainFeatures.LargeTerrainFeature>(loc.name, bush));
+    }
+  }
 
-  return String.Join("\n", bushes.Select(b => b.tilePosition.ToString()));
+  Game1.chatBox.addInfoMessage($"Found {blist.Count} bushes");
+  var bmaps = new List<string>(bmap.Keys);
+  bmaps.Sort();
+  var lines = new List<string>();
+  foreach (var mapname in bmaps) {
+    foreach (var feat in bmap[mapname]) {
+      lines.Add($"\tdebug warp {mapname} {feat.tilePosition.X} {feat.tilePosition.Y}");
+    }
+  }
+  return "\n" + String.Join("\n", lines) + "\n";
 }
